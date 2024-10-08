613ccb23426746001832e70bf29eb4bdd45aa551
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2024-10-08 20:58:02 -0300

    fix: ajustes no errorHandler

--- src/middlewares/errorHandling.ts
+++ src/middlewares/errorHandling.ts
@@ -10,12 +10,12 @@ export function errorHandler(
   __: NextFunction,
 ) {
   if (error instanceof ErrorMessage) {
-    return res.status(error.statusCode).json({
+    res.status(error.statusCode).json({
       message: error.message,
     });
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     message: error.message,
   });
 }
