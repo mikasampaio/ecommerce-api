f282448ac2eae4bf62de94314a1984b4324c2f3d
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2024-10-22 21:09:13 -0300

    config: cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express, { json } from 'express';
 
 import { mongoConfig } from './database';
 import { errorHandler } from './middlewares/errorHandling';
 import { routes } from './routes';
 
-mongoConfig().then(() => {
-  const app = express();
+const app = express();
 
+mongoConfig().then(() => {
+  app.use(cors());
   app.use(json());
   app.use(routes);
   app.use(errorHandler);
